849d0c18bc2624731abcd3f032f6a0bb2475547d
Geert <g.rozendom@newminds.nl>
2025-06-18 11:31:57 +0200

    Add service worker for offline caching

---
+++ service-worker.js
@@ -0,0 +1,61 @@
+const CACHE_NAME = 'saveloc-static-v1';
+const TILE_CACHE = 'saveloc-tiles-v1';
+const STATIC_ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './main.js',
+  './manifest.json',
+  './icons/icon-192x192.svg',
+  './icons/icon-512x512.svg',
+  './src/state.js',
+  './src/ui.js',
+  './src/storage.js',
+  './src/permission.js',
+  './src/map.js',
+  './src/data-layer.js',
+  './src/ui-controller.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(k => k !== CACHE_NAME && k !== TILE_CACHE).map(k => caches.delete(k))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+
+  if (url.hostname.match(/(tile\.openstreetmap\.org|basemaps\.cartocdn\.com|arcgisonline\.com)/)) {
+    event.respondWith(
+      caches.open(TILE_CACHE).then(cache => {
+        return cache.match(event.request).then(response => {
+          const fetchPromise = fetch(event.request).then(networkResp => {
+            cache.put(event.request, networkResp.clone());
+            return networkResp;
+          });
+          return response || fetchPromise;
+        });
+      })
+    );
+    return;
+  }
+
+  if (event.request.method === 'GET' && url.origin === location.origin) {
+    event.respondWith(
+      caches.match(event.request).then(resp => resp || fetch(event.request))
+    );
+  }
+});
